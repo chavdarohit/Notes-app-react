f10b8e9dfa4fc317d0ccf18bf4f3a9cb1fdd4b14
rohit <rohitchavda449@gmail.com>
2023-07-12 11:25:27 +0530

    v1 creating note

---
+++ src/Components/Note/Note.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Note.css';
+
+
+function Note(props){
+return <div className="note">
+    <textarea className="note_text"></textarea>
+    <p>11:50 PM 22 April</p>
+     </div>
+
+}
+
+export default Note;
